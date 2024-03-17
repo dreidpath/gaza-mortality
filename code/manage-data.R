62bdd73cd218c859b40a03b8104cee5f14f30cd0
Daiel D. Reidpath <daniel.reidpath@gmail.com>
2024-03-17 23:51:51 +0100

    The extrapolation of women and children deaths at the end of the series has been altered. On the tracker site total deaths can increase but separate counts for women and children are not available. The counts are extrapolated using the proportion of women/children deaths based on the last time they were updated.

--- code/manage-data.R
+++ code/manage-data.R
@@ -13,6 +13,26 @@ deathDF$report_date <- dmy(deathDF$report_date)
 deathDF$g_dead_women[is.na(deathDF$g_dead_women)] <- 0
 deathDF$g_dead_child[is.na(deathDF$g_dead_child)] <- 0
 
+####################
+# The code here extrapolates the number of women and children who died by observing
+# The proportion of women and children who died at the last time women and children
+# deaths were updated. The proportion is then multiplied by the increasing total deaths
+# as an extrapolated estimate of how the women and children deaths should be increasing
+
+# Extrapolate women's deaths
+deathDF$g_dead_womenx <- deathDF$g_dead_women
+last_change <- lastchange(deathDF$g_dead_womenx)
+extrapolate_prop <- deathDF$g_dead_womenx[last_change]/deathDF$g_dead_total[last_change]
+deathDF$g_dead_womenx[last_change:length(deathDF$g_dead_womenx)] <- floor(deathDF$g_dead_total[last_change:length(deathDF$g_dead_womenx)] * extrapolate_prop)
+# Extrapolate children's deaths  
+deathDF$g_dead_childx <- deathDF$g_dead_child
+last_change <- lastchange(deathDF$g_dead_childx)
+extrapolate_prop <- deathDF$g_dead_child[last_change]/deathDF$g_dead_total[last_change]
+deathDF$g_dead_childx[last_change:length(deathDF$g_dead_childx)] <- floor(deathDF$g_dead_total[last_change:length(deathDF$g_dead_childx)] * extrapolate_prop)
+#clean up
+rm(list = c("extrapolate_prop", "last_change", "lastchange"))
+###################
+
 # Model the death count for days without updates
 # Calculate the deaths on each day as the difference between one day and the day before
 deathDF$g_dailydead_total <- c(deathDF$g_dead_total[1], diff(deathDF$g_dead_total))
@@ -23,7 +43,7 @@ deathDF$mg_dead_total <- cumsum(deathDF$mg_dailydead_total)
 
 ## Model the child deaths (this is an "at least figure")
 # Step 1: Model the proportion of dead each day who are children
-deathDF$mg_prop_childdeaths <- model_missing_proportions(deathDF$g_dead_child, deathDF$mg_dead_total)
+deathDF$mg_prop_childdeaths <- model_missing_proportions(deathDF$g_dead_childx, deathDF$mg_dead_total)
 # Step 2: multiply the proportion by the total dead to estimate child daily deaths
 deathDF$mg_dead_child <- deathDF$mg_prop_childdeaths * deathDF$mg_dead_total
 # Step 3: Model daily child deaths
@@ -31,7 +51,7 @@ deathDF$mg_daily_childdeaths <- c(deathDF$mg_dead_child[1], diff(deathDF$mg_dead
 
 ## Model the women deaths (this is an "at least figure")
 # Step 1: Model the proportion of dead each day who are women
-deathDF$mg_prop_womendeaths <- model_missing_proportions(deathDF$g_dead_women, deathDF$mg_dead_total)
+deathDF$mg_prop_womendeaths <- model_missing_proportions(deathDF$g_dead_womenx, deathDF$mg_dead_total)
 # Step 2: multiply the proportion by the total dead to estimate women daily deaths
 deathDF$mg_dead_women <- deathDF$mg_prop_womendeaths * deathDF$mg_dead_total
 # Step 3: Model daily women deaths
